5ce92ce98b2037279a3fd9664b360b47cc31c80c
Kar Epker <karepker@gmail.com>
2023-09-12 16:38:11 -0700

    Add full aliases for gvim and gvimdiff.
    Also add a note indicating how to get around this for applications that don't use the full shell configuration, e.g. git.

--- .config/zsh/macos.zsh
+++ .config/zsh/macos.zsh
@@ -1,2 +1,10 @@
 alias ls='ls -Ghf'
-alias gvim='mvim'
+# Note: These aliases work well in the shell, but aren't necessarily picked up
+# by, e.g. git config. For those, it's probably best to symlink the binaries,
+# e.g.
+#
+# $ ln -s /Applications/MacVim.app/Contents/bin/gvim ~/bin/gvimdiff
+#
+# ~/bin should already be made part of PATH by base.zsh.
+alias gvim='/Applications/MacVim.app/Contents/bin/mvim'
+alias gvimdiff='/Applications/MacVim.app/Contents/bin/mvimdiff'
